5b6f4e2bc7c039663716ca5d6947c80c5b4983a1
unknown <you@example.com>
2022-05-07 18:56:26 +0100

    create job interface and integrate job data

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,48 @@
 <template>
-  <div>
-    <div class="app">
-      <p>{{ name }} - {{ age }}</p>
-    </div>
-  <button @click="changeName('nejah')">change Name</button>
-  <button @click="changeAge('30')">change age</button>
+  <div class="app">
+    <job-list :jobs="jobs"/>
   </div>
-
 </template>
 
-<script lang="ts" >
-     
-import { defineComponent,reactive ,toRefs,ref} from "vue";
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+import { Job } from "./types/job";
+import JobList from './components/jobList.vue'
 
 export default defineComponent({
-  name: 'App',
-  components: {},
-  setup(){
-    //  const state = reactive({
-    //    name :'link' ,
-    //    age : 23 as string | number
-    //  })
-    //    return{...toRefs(state)}
-     const name = ref('amin')
-     const age = ref(27)
-    return { name , age}
+  name: "App",
+  components: {JobList},
+  setup() {
+    const jobs = ref<Job[]>([
+      {
+        title: "farm worker",
+        location: "lon lon ranch",
+        salary: 30000,
+        id: "1",
+      },
+      {
+        title: "quarryman",
+        location: "death mountain",
+        salary: 40000,
+        id: "2",
+      },
+      {
+        title: "flute player",
+        location: "the lost woods",
+        salary: 35000,
+        id: "3",
+      },
+      { title: "fisherman", location: "lake hylia", salary: 21000, id: "4" },
+      {
+        title: "prison guard",
+        location: "gerudo valley",
+        salary: 32000,
+        id: "5",
+      },
+    ]);
+
+    return { jobs };
   },
-  // data() {
-  //   return {
-  //     name: 'Link',
-  //     age: 25 as number | string
-  //   }
-  // },
-  methods: {
-    changeName(name: string) {
-      this.name = name
-      return name
-    },
-    changeAge(age: number | string) {
-      this.age = age
-      return age
-    }
-  }
 });
 </script>
 
